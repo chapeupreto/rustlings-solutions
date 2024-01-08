a2c33e14bba2c87b0753546f8b9d9b8a5da1b788
Rod Elias <rodiney@gmail.com>
2024-01-08 20:57:15 -0300

    feat: add exercises/ folder with solutions

---
+++ exercises/clippy/target/debug/deps/clippy3-32c0bbc14e957769.d
@@ -0,0 +1,9 @@
+/home/rodiney/rustlings/exercises/clippy/target/debug/deps/libclippy3-32c0bbc14e957769.rmeta: clippy3.rs Cargo.toml
+
+/home/rodiney/rustlings/exercises/clippy/target/debug/deps/clippy3-32c0bbc14e957769.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
